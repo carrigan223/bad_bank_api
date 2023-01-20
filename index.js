da0ad87bced7291d20be3254a1c8f5dcb35757d5
Andrew Carrigan <carrigan223@gmail.com>
2023-01-20 10:17:30 -0800

    added get to firsetore

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 //bring in expres from express
 const express = require("express");
+//imort admin from firebase-admin
+const admin = require("firebase-admin");
+//bring in serviceAccount from serviceAccountKey.json
+const serviceAccount = require("./config/firebaseCredentials.json");
 
+//initialize firebase admin with serviceAccount and databaseURL
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://office-hour-firebase.firebaseio.com",
+});
+
+//create a database variable
+const db = admin.firestore();
 //create an instance of express
 const app = express();
 
-//create a route
+/////////////ROUTES/////////////
+
+//route to get all users
+app.get("/users", (req, res) => {
+  //create a users array
+  let users = [];
+  //get all users from the database
+  const usersRef = db.collection("users");
+  usersRef.get().then((snapshot) => {
+    //loop through the users
+    snapshot.forEach((doc) => {
+      //push the user to the users array
+      users.push(doc.data().first_name);
+    });
+    //send the users array
+    res.send(users);
+  });
+});
+
+//base` route
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Pat");
 });
 
 //create a port
